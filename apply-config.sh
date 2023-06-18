9170d16802ba8e242d77f5636aea46a4f73074f9
grigoran <igormin81@mail.ru>
2023-06-18 18:15:55 +0400

    base

---
+++ apply-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r sway $HOME/.config/
+cp -r waybar $HOME/.config/
+
+swaymsg reload
---
+++ save-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#copy config files
+cp -r $HOME/.config/sway ./
+cp -r $HOME/.config/waybar ./
